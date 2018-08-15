8d516c948176ffb53f9a2cc5820ee403dafa450f
Arun <arunkn28@gmail.com>
2018-08-15 21:38:44 +0530

    Added requirements.txt

--- U3/settings.py
+++ U3/settings.py
@@ -76,13 +76,22 @@ WSGI_APPLICATION = 'U3.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'u3',
+        'USER': 'root',
+        'PASSWORD': 'Welcome@29',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3307',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
